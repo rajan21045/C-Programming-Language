09143e11d80313273676f14bad1395e2d5059659
Rajan Poudel <raajan.works@gmail.com>
2025-05-18 11:19:11 +0545

    Update strcmpexampple.c

--- Strings/String Functions/strcmpexampple.c
+++ Strings/String Functions/strcmpexampple.c
@@ -20,11 +20,22 @@ int main(){
 	else{
 		printf("Text2 Come First.");
 	}
-	
-	
-	
-	
-	
-	
+
 	return 0;
 }
+
+/*
+strcmp(string1, string2):-
+
+- This string comparison is used to compare two string components. The output of comparison is an integer value either positive (+) or negative (-) or equal to (0,0) zero.
+- The output of this function is zero if string1 and string2 are equal.
+- The output of this function is positive if string1 has higher character than string2.
+- The output of this function is negative if string1 has lower character than string2.
+- The comparison is performed in chronological order - like dictionary.
+
+eg:
+char string1[] = "computer";
+    char string2[] = "science";
+    strcmp(string1, string2); // returns -1
+    
+*/
