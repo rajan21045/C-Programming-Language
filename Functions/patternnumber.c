ebe879822c62e081efc9a73b04cc7ec752ebcf2f
Rajan Poudel <raajan.works@gmail.com>
2025-05-19 20:13:17 +0545

    Update patternnumber.c

--- Functions/patternnumber.c
+++ Functions/patternnumber.c
@@ -10,15 +10,15 @@ Write a program to generate the following pattern using loops:
 
 */
 
-
 #include <stdio.h>// Include the standard input-output library for using printf and scanf functions
 #include <stdlib.h> // Include the standard library for using exit function
 
 int main() { // Main function where the program execution starts
+    
     int rows; // Variable to store the number of rows for the pattern
     printf("Enter the number of rows: "); // Prompt the user to enter the number of rows for the pattern
-    
     scanf("%d", &rows);  // Read the number of rows from the user
+    
     for(int i = 1; i <= rows; i++) { // Loop to iterate through each row from 1 to the number of rows
         for(int j = 1; j <= i; j++) { // Loop to print the number of columns in each row
                 printf("1 \t"); // Print 1 for each column in the row
