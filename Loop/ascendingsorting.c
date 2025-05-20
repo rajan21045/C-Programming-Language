d834371263cafd2bd39c16b26ff6af70df719db7
Rajan Poudel <raajan.works@gmail.com>
2025-05-20 09:30:46 +0545

    Add files via upload

---
+++ Loop/ascendingsorting.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    int i, j, temp, n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements of the array: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Original array: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // Bubble sort in ascending order
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted array in ascending order: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
