c81926eee15449bb9367cc363b266c0f9e5a36f7
Rajan Poudel <raajan.works@gmail.com>
2025-05-28 16:38:46 +0545

    Add files via upload

---
+++ Arrays/largeElementInArray.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(){
+	int i,n,arr[20],max;
+	printf("Enter The Number Upto Which Element Should Be: ");
+	scanf("%d",&n);
+	
+	printf("Enter The Element: ");
+	for(i =0; i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	max = arr[0];
+	for(i =0; i<n; i++){
+		if(arr[i]>max){
+			max = arr[i];
+		}
+	}
+	
+	printf("Largest Element In The Array Is %d",max);
+	
+	
+	
+	
+	return 0;
+}
