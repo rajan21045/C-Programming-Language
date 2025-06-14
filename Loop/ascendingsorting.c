9960741593fa8630fa0a93fcd0fe0de1eedbdece
Rajan Poudel <raajan.works@gmail.com>
2025-06-14 16:00:27 +0545

    Update ascendingsorting.c

--- Loop/ascendingsorting.c
+++ Loop/ascendingsorting.c
@@ -16,22 +16,28 @@ int main() {
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
+    
+    
     printf("\n");
 
     // Bubble sort in ascending order
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
+                
                 // Swap
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                
             }
         }
     }
 
     printf("Sorted array in ascending order: ");
+    
     for (i = 0; i < n; i++) {
+        
         printf("%d ", arr[i]);
     }
     printf("\n");
