171416660a238be348955f81fb4e51d91149ea0b
Rajan Poudel <raajan.works@gmail.com>
2025-08-08 21:03:00 +0545

    Add files via upload

---
+++ Arrays/arraySortUsingPointer.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+	int arr[] = {1,2,6,3,4};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	sortArr(arr,n);
+	printArr(arr, n);
+}
+
+void sortArr(int *arr, int n){
+	int i,j,temp;
+	for(i=0; i<n-1; i++){
+		for(j=0; j<n-1-i; j++){
+			if(*(arr+ j)> *arr +j+1 ){
+				temp = * (arr + j);
+				*(arr+j) = *(arr +j+1);
+				*(arr+ j+1) = temp; 
+			}
+		}
+	}
+}
+
+
+void printArr(int *arr, int n){
+	int i;
+	for(i=0; i<n; i++){
+		printf("%d ", *(arr+ i));
+	}
+}
